fddab1363b67f78071919e8de7ab5ae047c20d69
cpusillo <cpusillo@gmail.com>
2024-04-11 13:03:49 -0400

    Add additional notes

--- API_to_CSV/utils/extract.py
+++ API_to_CSV/utils/extract.py
@@ -1,13 +1,15 @@
 import os, requests
 from dotenv import load_dotenv
 
+# Load our env variables
+# Make sure you're storing your API keys in a .env file!
 load_dotenv()
 
 
 def get_weather_data():
     # Pulls the data from the weather API
     city = "New York"
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={os.getenv("OPEN_WEATHER_API_KEY")}'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={os.getenv("OPEN_WEATHER_API_KEY")}' #os.getenv("API_KEY_NAME_HERE") pulls our API key for us.
 
     try:
         response = requests.get(url)
